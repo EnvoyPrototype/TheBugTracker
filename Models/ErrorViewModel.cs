a71803acb0dceec07ee8978c7efd9c37a0b31f55
Jim Rogers <thejimrogers@pm.me>
2021-12-15 15:29:49 -0600

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheBugTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
