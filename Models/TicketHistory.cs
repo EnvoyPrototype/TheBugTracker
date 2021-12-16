9454055daefed81a0ce7ee8810c2c1e3b66dfe92
Jim Rogers <thejimrogers@pm.me>
2021-12-16 08:39:33 -0600

    Data Models / TicketHistory

---
+++ Models/TicketHistory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class TicketHistory
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("Updated Item")]
+        public string Property { get; set; }
+
+        [DisplayName("Previous")]
+        public string OldValue { get; set; }
+
+        [DisplayName("Current")]
+        public string NewValue { get; set; }
+
+        [DisplayName("Date Modified")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Description of Change")]
+        public string Description { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        // Navigation properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+    }
+}
