c56feec1a5f767931a88cfa77b766f44e041f170
Jim Rogers <thejimrogers@pm.me>
2021-12-16 20:45:41 -0600

    Data Models - TicketAttachment

---
+++ Models/TicketAttachment.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class TicketAttachment
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Ticket")]
+        public int TicketId { get; set; }
+
+        [DisplayName("File Date")]
+        public DateTimeOffset Created { get; set; }
+
+        [DisplayName("Team Member")]
+        public string UserId { get; set; }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile FormFile { get; set; }
+
+        [DisplayName("File Name")]
+        public string FileName { get; set; }
+
+        public byte[] FileData { get; set; }
+
+        [DisplayName("File Extension")]
+        public string FileContentType { get; set; }
+
+        // Navigation properties
+        public virtual Ticket Ticket { get; set; }
+        public virtual BTUser User { get; set; }
+    }
+}
