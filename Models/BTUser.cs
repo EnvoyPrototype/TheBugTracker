45ba856df825e34f07668e8cde8638b1409d137e
Jim Rogers <thejimrogers@pm.me>
2021-12-17 08:10:03 -0600

    Data Models - Project Members

--- Models/BTUser.cs
+++ Models/BTUser.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -21,5 +23,24 @@ namespace TheBugTracker.Models
         [NotMapped]
         [Display(Name = "Full Name")]
         public string FullName { get { return $"{FirstName} {LastName}"; } }
+
+        [NotMapped]
+        [DataType(DataType.Upload)]
+        public IFormFile AvatarFormFile { get; set; }
+
+        [DisplayName("Avatar")]
+        public string AvatarFileName { get; set; }
+
+        public byte[] AvatarFileData { get; set; }
+
+        [Display(Name = "File Extension")]
+        public string AvatarContentType { get; set; }
+
+        public int? CompanyId { get; set; }
+
+        // Navigation properties
+        public virtual Company Company { get; set; }
+
+        public virtual ICollection<Project> Projects { get; set; }
     }
 }
