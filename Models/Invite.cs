c9ea37f4ee0f357928054c67fcef7839b94d45d0
Jim Rogers <thejimrogers@pm.me>
2021-12-17 08:48:35 -0600

    Data Models - Invite

---
+++ Models/Invite.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class Invite
+    {
+        // Primary key
+        public int Id { get; set; }
+
+        [DisplayName("Date Sent")]
+        public DateTimeOffset InviteDate { get; set; }
+
+        [DisplayName("Join Date")]
+        public DateTimeOffset JoinDate { get; set; }
+
+        [DisplayName("Code")]
+        public Guid CompanyToken { get; set; }
+
+        [DisplayName("Company")]
+        public int CompanyId { get; set; }
+
+        [DisplayName("Project")]
+        public int ProjectId { get; set; }
+
+        [DisplayName("Invitor")]
+        public string InvitorId { get; set; }
+
+        [DisplayName("Invitee")]
+        public string InviteeId { get; set; }
+
+        [DisplayName("Invitee Email")]
+        public string InviteeEmail { get; set; }
+
+        [DisplayName("Invitee First Name")]
+        public string InviteeFirstName { get; set; }
+
+        [DisplayName("Invitee Last Name")]
+        public string InviteeLastName { get; set; }
+
+        public bool IsValid { get; set; }
+
+        // Navigation properties
+        public virtual Company Company { get; set; }
+        public virtual BTUser Invitor { get; set; }
+        public virtual BTUser Invitee { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
