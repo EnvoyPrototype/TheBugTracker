7eab5aeb5c078835d93cfebf42eff1b5cd7e6b4f
Jim Rogers <thejimrogers@pm.me>
2021-12-17 07:39:01 -0600

    Data Models - Project & Project Priority

---
+++ Models/ProjectPriority.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models
+{
+    public class ProjectPriority
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Priority Name")]
+        public string Name { get; set; }
+    }
+}
