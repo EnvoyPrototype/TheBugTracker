6155885cd47bff104c80065297ea1fe9dd77fb8b
Jim Rogers <thejimrogers@pm.me>
2021-12-24 08:57:54 -0600

    Create + register Ticket History Service + interface

---
+++ Services/Interfaces/IBTTicketHistoryService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBugTracker.Models;
+
+namespace TheBugTracker.Services.Interfaces
+{
+    public interface IBTTicketHistoryService
+    {
+        Task AddHistoryAsync(Ticket oldTicket, Ticket newTicket, string userId);
+
+        Task<List<TicketHistory>> GetProjectTicketsHistoriesAsync(int projectId, int companyId);
+
+        Task<List<TicketHistory>> GetCompanyTicketsHistoriesAsync(int companyId);
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -46,6 +46,7 @@ namespace TheBugTracker
             services.AddScoped<IBTCompanyInfoService, BTCompanyInfoService>();
             services.AddScoped<IBTProjectService, BTProjectService>();
             services.AddScoped<IBTTicketService, BTTicketService>();
+            services.AddScoped<IBTTicketHistoryService, BTTicketHistoryService>();
 
             services.AddControllersWithViews();
         }
