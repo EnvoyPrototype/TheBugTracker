5265c18392ec5f35facda3bb9edaae3f01a449c1
Jim Rogers <hello@jimrogers.dev>
2022-03-25 00:24:47 -0500

    Role Management: View

--- Controllers/UserRolesController.cs
+++ Controllers/UserRolesController.cs
@@ -23,6 +23,7 @@ namespace TheBugTracker.Controllers
             _companyInfoService = companyInfoService;
         }
 
+        [HttpGet]
         public async Task<IActionResult> ManageUserRoles()
         {
             // Add an instance of the ViewModel as a List (model)
