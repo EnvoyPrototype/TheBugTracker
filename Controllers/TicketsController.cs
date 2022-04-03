42cee5be55bf15226ab82d2eacc2a1650f5f167c
Jim Rogers <hello@jimrogers.dev>
2022-04-02 23:41:09 -0500

    Ticket Features: My Tickets View

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -42,6 +42,15 @@ namespace TheBugTracker.Controllers
             return View(await applicationDbContext.ToListAsync());
         }
 
+        public async Task<IActionResult> MyTickets()
+        {
+            BTUser btUser = await _userManager.GetUserAsync(User);
+
+            List<Ticket> tickets = await _ticketService.GetTicketsByUserIdAsync(btUser.Id, btUser.CompanyId);
+
+            return View(tickets);
+        }
+
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)
         {
