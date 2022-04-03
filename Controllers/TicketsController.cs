91aff6f5c582e3f5dc1735ac7491b0c2fe5c3a75
Jim Rogers <hello@jimrogers.dev>
2022-04-03 08:57:38 -0500

    Ticket Features: All Tickets: DEBUGGING

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -51,6 +51,22 @@ namespace TheBugTracker.Controllers
             return View(tickets);
         }
 
+        public async Task<IActionResult> AllTickets()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Ticket> tickets = await _ticketService.GetAllTicketsByCompanyAsync(companyId);
+
+            if (User.IsInRole(nameof(Roles.Developer)) || User.IsInRole(nameof(Roles.Submitter)))
+            {
+                return View(tickets.Where(t => t.Archived == false));
+            }
+            else
+            {
+                return View(tickets);
+            }
+        }
+
         // GET: Tickets/Details/5
         public async Task<IActionResult> Details(int? id)
         {
