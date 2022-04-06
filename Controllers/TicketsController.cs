ce20402152ce2e33a3238a4534f1353b1c0bd0ae
Jim Rogers <hello@jimrogers.dev>
2022-04-05 23:19:16 -0500

    Ticket Management Feature / Ticket Assignment - GET Method

--- Controllers/TicketsController.cs
+++ Controllers/TicketsController.cs
@@ -12,6 +12,7 @@ using TheBugTracker.Data;
 using TheBugTracker.Extensions;
 using TheBugTracker.Models;
 using TheBugTracker.Models.Enums;
+using TheBugTracker.Models.ViewModels;
 using TheBugTracker.Services.Interfaces;
 
 namespace TheBugTracker.Controllers
@@ -107,6 +108,19 @@ namespace TheBugTracker.Controllers
 
                 return View(pmTickets);
             }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AssignDeveloper(int id)
+        {
+            AssignDeveloperViewModel model = new();
+
+            model.Ticket = await _ticketService.GetTicketByIdAsync(id);
+            model.Developers = new SelectList(await _projectService.GetProjectMembersByRoleAsync(model.Ticket.Id,
+                nameof(Roles.Developer)),
+                "Id", "FullName");
+
+            return View(model);
         }
 
         // GET: Tickets/Details/5
---
+++ Models/ViewModels/AssignDeveloperViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models.ViewModels
+{
+    public class AssignDeveloperViewModel
+    {
+        public Ticket Ticket { get; set; }
+        public SelectList Developers { get; set; }
+        public string DeveloperId { get; set; }
+    }
+}
