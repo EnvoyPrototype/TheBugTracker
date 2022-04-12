b88abb599da56ace8f8570c25a4aa669ddf57bc7
Jim Rogers <hello@jimrogers.dev>
2022-04-12 16:34:20 -0500

    Dashboard - GET Method & ViewModel

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,22 +5,45 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using TheBugTracker.Extensions;
 using TheBugTracker.Models;
+using TheBugTracker.Models.ViewModels;
+using TheBugTracker.Services.Interfaces;
 
 namespace TheBugTracker.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IBTCompanyInfoService _companyInfoService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,
+                              IBTCompanyInfoService companyInfoService)
         {
             _logger = logger;
+            _companyInfoService = companyInfoService;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> Dashboard()
+        {
+            DashboardViewModel model = new();
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            model.Company = await _companyInfoService.GetCompanyInfoByIdAsync(companyId);
+            model.Projects = (await _companyInfoService.GetAllProjectsAsync(companyId))
+                                                       .Where(p => p.Archived == false)
+                                                       .ToList();
+            model.Tickets = model.Projects.SelectMany(p => p.Tickets)
+                                          .Where(t => t.Archived == false)
+                                          .ToList();
+            model.Members = model.Company.Members.ToList();
+
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBugTracker.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public Company Company { get; set; }
+        public List<Project> Projects { get; set; }
+        public List<Ticket> Tickets { get; set; }
+        public List<BTUser> Members { get; set; }
+    }
+}
