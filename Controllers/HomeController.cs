c4073de3f834f80d1e018e466d6c7c6acd2e987d
Jim Rogers <hello@jimrogers.dev>
2022-04-13 09:09:36 -0500

    Dashboard: Google Charts: Pie/Donut Charts

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using TheBugTracker.Extensions;
 using TheBugTracker.Models;
 using TheBugTracker.Models.ViewModels;
 using TheBugTracker.Services.Interfaces;
+using TheBugTracker.Models.Enums;
 
 namespace TheBugTracker.Controllers
 {
@@ -16,12 +17,15 @@ namespace TheBugTracker.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBTCompanyInfoService _companyInfoService;
+        private readonly IBTProjectService _projectService;
 
         public HomeController(ILogger<HomeController> logger,
-                              IBTCompanyInfoService companyInfoService)
+                              IBTCompanyInfoService companyInfoService, 
+                              IBTProjectService projectService)
         {
             _logger = logger;
             _companyInfoService = companyInfoService;
+            _projectService = projectService;
         }
 
         public IActionResult Index()
@@ -55,6 +59,44 @@ namespace TheBugTracker.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> GglProjectTickets()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Project> projects = await _projectService.GetAllProjectsByCompany(companyId);
+
+            List<object> chartData = new();
+            chartData.Add(new object[] { "ProjectName", "TicketCount" });
+
+            foreach (Project prj in projects)
+            {
+                chartData.Add(new object[] { prj.Name, prj.Tickets.Count() });
+            }
+
+            return Json(chartData);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> GglProjectPriority()
+        {
+            int companyId = User.Identity.GetCompanyId().Value;
+
+            List<Project> projects = await _projectService.GetAllProjectsByCompany(companyId);
+
+            List<object> chartData = new();
+            chartData.Add(new object[] { "Priority", "Count" });
+
+
+            foreach (string priority in Enum.GetNames(typeof(BTProjectPriority)))
+            {
+                int priorityCount = (await _projectService.GetAllProjectsByPriority(companyId, priority)).Count();
+                chartData.Add(new object[] { priority, priorityCount });
+            }
+
+            return Json(chartData);
         }
     }
 }
